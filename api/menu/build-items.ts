e0bb0ddab8100f8ee1cfab85929866d41acef57f
Tyler Holewinski <tyler.holewinski@bryx.com>
2022-08-13 23:17:18 -0400

    refactor(menuitems): move menu items to standalone json

---
+++ api/menu/build-items.ts
@@ -0,0 +1,19 @@
+import { ensureConnected } from "../src/db/util";
+import MenuItem from "../src/db/models/MenuItem";
+
+import { MenuItemSpec } from "../src/db/schemas/MenuItem";
+import items from "./items.json";
+
+(async () => {
+  console.log("☁️  Connecting to mongo.erwijet.com...");
+  await ensureConnected();
+  console.log("⚙️  Purging old items...");
+  await MenuItem.deleteMany({});
+  for (let item of items as MenuItemSpec[]) {
+    await new MenuItem(item).save();
+    console.log("👉 Inserted " + item.name);
+  }
+  console.log("🎉 Rebuilt MenuItem Collection");
+
+  process.exit(0);
+})();
