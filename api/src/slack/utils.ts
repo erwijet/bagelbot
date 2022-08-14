800bc3be29c5e61d577b3650ff26d4d63d324405
Tyler Holewinski <tyler.holewinski@bryx.com>
2022-08-13 22:47:23 -0400

    feat(slack/util): add non-response based send message functionality

--- api/src/slack/utils.ts
+++ api/src/slack/utils.ts
@@ -8,3 +8,13 @@ export function sendInteractionResponse(responseUrl: string, content: string) {
     }),
   });
 }
+
+export function sendMessage(content: string, hidden?: boolean) {
+  return fetch("https://hooks.slack.com/services/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", {
+    method: "POST",
+    body: JSON.stringify({
+      text: content,
+      ephemeral: !!hidden,
+    }),
+  });
+}
